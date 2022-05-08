eff5af3f2766544216e5584147b26525e390d38a
JohnCruz <john.cruz6725@gmail.com>
2022-05-08 14:53:12 -0500

    quick fix

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'resume_project.info_api'
+    'Rem_Site_Hero.resume_project.info_api'
 ]
 
 MIDDLEWARE = [
