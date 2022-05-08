eca2c4ce43d808b5bc248aa11bf8c892d20bfae4
JohnCruz <john.cruz6725@gmail.com>
2022-05-08 14:13:31 -0500

    fix

--- info_api/views.py
+++ info_api/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from serializers import *
+from mySserializers import *
 
 
 class PersonViewSet(viewsets.ModelViewSet):
