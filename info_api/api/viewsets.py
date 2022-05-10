2a1d0583ba8809010626c0db1f0293df88449e46
JohnCruz <john.cruz6725@gmail.com>
2022-05-10 17:09:13 -0500

    permision change

--- info_api/api/viewsets.py
+++ info_api/api/viewsets.py
@@ -2,7 +2,6 @@ from info_api.models import Person
 from  .serializers import PersonSerializer
 from rest_framework import viewsets
 
-class PersonViewSet(viewsets.ModelViewSet):
+class PersonViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = [IsAccountAdminOrReadOnly]
