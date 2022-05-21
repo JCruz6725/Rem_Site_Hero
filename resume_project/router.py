c5fb17002bfcef27b81d4666f3c5f2ec6d024f4a
JohnCruz <john.cruz6725@gmail.com>
2022-05-20 20:09:28 -0500

    remove unneeded viewsets

--- resume_project/router.py
+++ resume_project/router.py
@@ -3,8 +3,11 @@ from info_api.api.viewsets import *
 
 router = routers.DefaultRouter()
 router.register(r'person', PersonViewSet)
+
+'''
+
 router.register(r'resume', ResumeViewSet)
 router.register(r'education', EducationViewSet)
 router.register(r'professional', ProfessionalViewSet)
 router.register(r'project', ProjectViewSet)
-
+'''
