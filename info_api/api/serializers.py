982d6c0e1c58727005d676b80ebd5e1fde3026e6
JohnCruz <john.cruz6725@gmail.com>
2022-06-04 12:25:09 -0500

    .

--- info_api/api/serializers.py
+++ info_api/api/serializers.py
@@ -47,7 +47,7 @@ class UserSerializer (serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ['username', 'first_name', 'last_name', 'email']
 
 
  
