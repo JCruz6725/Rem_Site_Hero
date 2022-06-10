f58cb4b52cef3d090294e7b94ec7091e658c4a80
JohnCruz <john.cruz6725@gmail.com>
2022-06-09 22:32:54 -0500

    admin menu rework File:info_api.admin

--- info_api/admin.py
+++ info_api/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+
 from .models import Person, Resume, Education, Project, Professional, Account
 
-admin.site.register(Account, UserAdmin)
+class AccountAdmin (BaseUserAdmin):
+    list_display = ('email', 'username', 'date_joined',  'is_admin')
+    search_fields = ('email')
+    readonly_fields = ('data_joined', 'last_login')
+
+
+    list_filter = ()
+    fieldsets = ()
+    filter_horizontal = ()
+
+    
+
+admin.site.register(Account, AccountAdmin)
 
 admin.site.register(Person)
 admin.site.register(Resume)
